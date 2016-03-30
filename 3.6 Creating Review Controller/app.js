dba78b32d1536fa0b97f06441cd254d814700e26
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-03-30 17:01:44 +0800

    3.6 Done

--- 3.6 Creating Review Controller/app.js
+++ 3.6 Creating Review Controller/app.js
@@ -25,7 +25,13 @@
     };
   });
 
-  
+  app.controller('ReviewController', function(){
+    this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
   var gems = [
     {
