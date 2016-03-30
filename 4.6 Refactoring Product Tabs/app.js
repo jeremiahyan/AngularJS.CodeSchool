0bd1f85aa8842e330654b57ccf8f121b75f27610
Jeremiah Yan <jeremiah.yxj@gmail.com>
2016-03-31 01:55:51 +0800

    4.6 Done

--- 4.6 Refactoring Product Tabs/app.js
+++ 4.6 Refactoring Product Tabs/app.js
@@ -12,19 +12,24 @@
     this.products = gems;
   });
 
-  app.controller("TabController", function() {
-    this.tab = 1;
+  app.directive("productTabs", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function(){
+        this.tab = 1;
 
-    this.isSet = function(checkTab) {
-      return this.tab === checkTab;
-    };
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
 
-    this.setTab = function(setTab) {
-      this.tab = setTab;
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+      },
+      controllerAs: 'tab'
     };
   });
-
-  
 
   app.controller("ReviewController", function(){
 
